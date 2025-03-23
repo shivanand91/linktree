5de3c7a00df5f7c95d33978e88ef59311883b9f4
Shivanand Kumar <132545822+shivanand91@users.noreply.github.com>
2025-03-23 12:33:37 +0530

    second commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import { Inter, Roboto_Mono } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+const robotoMono = Roboto_Mono({
+  variable: "--font-roboto-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "LinkTree - Home",
+  description: "LinkTree clone built with Next.js",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.variable} ${robotoMono.variable} antialiased`}
+      >
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <section className="bg-[#254F1A] h-screen ">
+
+      </section>
+      <section className="bg-red-500 h-screen ">
+
+      </section>
+    </main>
+  );
+}
